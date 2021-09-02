a8eafe77aad5ca50347178ca9da9a9076c94bc5d
Aleksey Plakhtiev <alekseyplakhtiev@gmail.com>
2021-09-02 20:56:09 +0300

    C# buns - How to measure the execution time of a code, function, program.

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyLibrary;
+using System.Diagnostics;
+using System.Threading;
 
 namespace MyProject
 {
@@ -12,8 +14,30 @@ namespace MyProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from MyProject!!!");
+
+            /**
+             * Использование собственной библиотеки
+             */
             double result = MyMath.Divide(3, 5);
             Console.WriteLine(result);
+
+            /**
+             * Как измерить время выполнения кода, метода, функции, программы.
+             */
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+            sw.Stop();
+            Console.WriteLine($"Time Spent: {sw.Elapsed} \nAnd milisecond: {sw.ElapsedMilliseconds}");
+            sw.Restart(); // тоже самое что и Reset() + Start()
+            Thread.Sleep(555);
+            sw.Stop();
+            Console.WriteLine($"Time Spent: {sw.Elapsed} \nAnd milisecond: {sw.ElapsedMilliseconds}");
+
+
             Console.ReadKey();
         }
     }
