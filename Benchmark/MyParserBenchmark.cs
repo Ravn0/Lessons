d061da4ef9b476e85f35a239dd376ac14f301c16
Aleksey Plakhtiev <alekseyplakhtiev@gmail.com>
2022-04-04 19:22:28 +0300

    Added a new Console .Net Core project Benchmark

---
+++ Benchmark/MyParserBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkLesson;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class MyParserBenchmark
+    {
+        private const string STRING_TO_PARSE_WITH_ERROR = "qwerty22";
+        private const string STRING_TO_PARSE = "54";
+        private readonly MyParser _myParser = new MyParser();
+        [Benchmark]
+        public void TryCatchParseTestWithError()
+        {
+            int result = _myParser.TryCathParse(STRING_TO_PARSE_WITH_ERROR);
+        }
+        [Benchmark]
+        public void TryParseTestWithError()
+        {
+            int result = _myParser.TryParse(STRING_TO_PARSE_WITH_ERROR);
+        }
+        [Benchmark]
+        public void TryParseFixedTestWithError()
+        {
+            int result = _myParser.TryParseFixed(STRING_TO_PARSE_WITH_ERROR);
+        }
+        [Benchmark]
+        public void TryCatchParseTest()
+        {
+            int result = _myParser.TryCathParse(STRING_TO_PARSE);
+        }
+        [Benchmark]
+        public void TryParseTest()
+        {
+            int result = _myParser.TryParse(STRING_TO_PARSE);
+        }
+        [Benchmark]
+        public void TryParseFixedTest()
+        {
+            int result = _myParser.TryParseFixed(STRING_TO_PARSE);
+        }
+    }
+}
---
+++ BenchmarkLesson/MyParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkLesson
+{
+    public class MyParser
+    {
+        public int TryCathParse(string str)
+        {
+            try
+            {
+
+                return int.Parse(str);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+        public int TryParse(string str)
+        {
+            int result;
+            if (!int.TryParse(str, out result))
+            {
+                result = 0;
+            }
+            return result;
+        }
+        public int TryParseFixed(string str)
+        {
+            int result;
+            int.TryParse(str, out result);
+            return result;
+        }
+    }
+}
